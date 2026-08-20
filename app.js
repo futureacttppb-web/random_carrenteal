c334f71a52f56405588e0d152088e4ed792be0b5
futureacttppb-web <futureacttppb@gmail.com>
2026-08-20 19:55:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ app.get('/book', (req, res) => {
   res.send('<h2>Book a Car</h2><p>Sorry we are having problems, 31-49U295824Y5924524524!</p><a href="/">Back to Home</a>');
 });
 app.get('/', (req, res) => {
-  console.log(req.ip); // shows the visitor’s IP
-  res.send('Hello visitor!');
+  // Check X-Forwarded-For header, fallback to connection remote address
+  const forwarded = req.headers['x-forwarded-for'];
+  const ip = forwarded ? forwarded.split(',')[0] : req.socket.remoteAddress;
+  
+  console.log(`Visitor IP: ${ip}`);
+  res.send('Hello!');
+});
 });
 
 
